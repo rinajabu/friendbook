972f8e855f1f1cb53a65356b6e5ed48f758dd80b
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro-2.local>
2023-01-24 10:12:20 -0500

    backend completed with routes, models, and controllers for users and posts

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -43,15 +43,14 @@ export const login = async (req, res) => {
 	try {
 		const { email, password } = req.body;
 		const user = await User.findOne({ email: email });
-		if (!user)
-			return res.status(400).json({ msg: "User does not exist. " });
+		if (!user) return res.status(400).json({ msg: "User does not exist." });
 
 		const isMatch = await bcrypt.compare(password, user.password);
 		if (!isMatch)
-			return res.status(400).json({ msg: "Invalid credentials. " });
+			return res.status(400).json({ msg: "Invalid credentials." });
 
 		const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-		delete user.password;
+		delete user.password; // deleting password so it isn't sent to the frontend
 		res.status(200).json({ token, user });
 	} catch (err) {
 		res.status(500).json({ error: err.message });
